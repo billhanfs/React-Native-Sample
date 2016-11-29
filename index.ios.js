2fdd6b684a475e1216cb43ae516e5728ca5c2043
Bill Han <billhanfs@outlook.com>
2016-11-29 12:12:08 +0800

    Props 2

--- index.ios.js
+++ index.ios.js
@@ -7,16 +7,26 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  Image,
+  Text,
+  View
 } from 'react-native';
+
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
 
 export default class SampleApp extends Component {
   render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
     return (
-      <Image source={pic} style={{width: 193, height: 110}}/>
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jania' />
+        <Greeting name='Valeera' />
+      </View>
     );
   }
 }
