cc46cd53337269eba8f4adea37b2d210ff58ec43
Bill Han <billhanfs@outlook.com>
2016-11-30 10:26:39 +0800

    State

--- index.ios.js
+++ index.ios.js
@@ -11,10 +11,20 @@ import {
   View
 } from 'react-native';
 
-class Greeting extends Component {
+class Blink extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {showText: true};
+
+    setInterval(() => {
+      this.setState({ showText: !this.state.showText });
+    }, 1000);
+  }
+
   render() {
+    let display = this.state.showText ? this.props.text : ' ';
     return (
-      <Text>Hello {this.props.name}!</Text>
+      <Text>{display}</Text>
     );
   }
 }
@@ -22,10 +32,11 @@ class Greeting extends Component {
 export default class SampleApp extends Component {
   render() {
     return (
-      <View style={{alignItems: 'center'}}>
-        <Greeting name='Rexxar' />
-        <Greeting name='Jania' />
-        <Greeting name='Valeera' />
+      <View>
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
       </View>
     );
   }
