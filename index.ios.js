fb6a2ded6594d01edb6221a5ef1f7021c31640fc
Bill Han <billhanfs@outlook.com>
2016-11-30 11:53:34 +0800

    Layout with Flexbox - Justify Content

--- index.ios.js
+++ index.ios.js
@@ -13,7 +13,11 @@ import {
 export default class SampleApp extends Component {
   render() {
     return (
-      <View style={{flex: 1, flexDirection: 'row'}}>
+      <View style={{
+        flex: 1, 
+        flexDirection: 'column', 
+        justifyContent: 'space-between',
+      }}>
         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
         <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
         <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
