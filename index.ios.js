f3168a64e74b84749dd042648565d6fb47bfb926
Bill Han <billhanfs@outlook.com>
2017-01-06 10:20:02 +0800

    Handling Text Input

--- index.ios.js
+++ index.ios.js
@@ -7,21 +7,28 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  Text,
+  TextInput,
   View
 } from 'react-native';
 
 export default class SampleApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
   render() {
     return (
-      <View style={{
-        flex: 1, 
-        flexDirection: 'column', 
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      <View style={{padding: 10}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+          onChangeText={(text) => this.setState({text})}
+        />
+        <Text style={{padding: 10, fontSize: 42}}>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+        </Text>
       </View>
     );
   }
